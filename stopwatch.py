f4a84ac2e9e067872f156fc6b71771e3322911ea
Markus.Privat <25428460+MarkusPrivat@users.noreply.github.com>
2026-03-08 01:22:33 +0100

    tutorial 3 done

--- stopwatch.py
+++ stopwatch.py
@@ -4,10 +4,20 @@ from textual.widgets import Footer, Header
 
 class Stopwatch(App):
     """A Textual app to manage stopwatches."""
+    BINDINGS = [
+        ("d", "toggle_dark_mode","Toggle dark mode")
+    ]
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header(show_clock=True)
         yield Footer()
+
+    def action_toggle_dark_mode(self):
+        """An action to toggle dark mode."""
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+
 
 if __name__ == '__main__':
     Stopwatch().run()
